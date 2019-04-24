7d4e5ea5ee349505748f8b24ba9f61d7453b413b
Martin Buchwald <martin.buchwald@mulesoft.com>
2019-04-24 16:58:25 -0300

    Compiling with java 7

--- btm/src/test/java/bitronix/tm/mock/resource/jdbc/MockDriver.java
+++ btm/src/test/java/bitronix/tm/mock/resource/jdbc/MockDriver.java
@@ -20,11 +20,9 @@
  */
 package bitronix.tm.mock.resource.jdbc;
 
-import java.sql.Driver;
-import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.DriverPropertyInfo;
+import java.sql.*;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 /**
  *
@@ -42,6 +40,11 @@ public class MockDriver implements Driver {
 
     public boolean jdbcCompliant() {
         return false;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
     }
 
     public boolean acceptsURL(String url) throws SQLException {
